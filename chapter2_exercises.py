28749b0a199c513e0cd9b335e1e0999b9d444122
mikekrieger <mlkrgr@gmail.com>
2013-07-17 11:39:06 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,26 @@
-# Exercises for chapter 2:
+#Name: Michael Krieger
 
-# Name:
+# Exercise 2.1 The number is expressed in octal form. 02492 is not recognized in octal 
+# because 9 is not recognized as a valid number. 1114 is the decimal equivalent of 
+# 02132 octal (2*8^3 + 1*8^2 + 3*8^1 + 2*8^0).
+#
+# Exercise 2.2 Responses are 5; nothing; 6 respectively. When they are print statements, 
+# they result in 5; syntaxerror; 6 respectively.
+#
+# Exercise 2.3 width/2 = 8; width/2.0 = 8.5; height/3 = 4.0; 1 + 2 * 5 = 11; 
+# delimiter *5 = '.....'
+#
+# Exercise 2.4.1 = 523.598775  
+r=5    
+v=(4.0/3.0)*3.14159265*r**3   
+print v   
+#
+# Exercise 2.4.2 = 945.45
+b=60
+o=(1.0-0.4)*24.95*b+0.75*b+2.25
+print o
+#
+# Exercise 2.4.3 = 38.1 minutes; 38 minutes 6 seconds; he gets home at 7:30 am
+e=8.25
+t=7.2
+2*e+3*t
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,101 @@
-# Exercises for chapter 3:
+# Name: Michael Krieger
 
-# Name:
+# Exercise 3.1
+repeat_lyrics()
+def print_lyrics(): 
+	print "I'm a lunberjack, and I'm okay." 
+	print "I sleep all night and I work all day"
+def repeat_lyrics(): 
+    print_lyrics() 
+    print_lyrics()
+
+#result: >>> repeat_lyrics()
+#Traceback (most recent call last):
+#  File "<stdin>", line 1, in <module>
+# NameError: name 'repeat_lyrics' is not defined
+#>>> def print_lyrics(): print "I'm a lunberjack, and I'm okay." print "I sleep all night and I work all day"
+#  File "<stdin>", line 1
+    def print_lyrics(): print "I'm a lunberjack, and I'm okay." print "I sleep all night and I work all day"
+                                                                    ^
+#SyntaxError: invalid syntax
+#>>> def repeat_lyrics(): print_lyrics() print_lyrics()
+
+#Exercise 3.2
+def repeat_lyrics(): 
+	print_lyrics() 
+	print_lyrics()
+def print_lyrics(): 
+	print "I'm a lunberjack, and I'm okay." 
+	print "I sleep all night and I work all day"
+repeat_lyrics()
+
+#result: SyntaxError: invalid syntax
+
+#Exercise 3.3
+str = "allen"
+print str.rjust(65, ' ');
+
+#Exercise 3.4.1
+def do_twice(f):
+     f()
+     f()
+
+def print_spam():
+     print 'spam'
+
+do_twice(print_spam)
+g
+
+#Exercise 3.4.2
+def do_twice(f, arg):
+    f(arg)
+    f(arg)
+
+def print_spam(arg):
+    print 'spam'
+
+do_twice(print_spam, 'spam')
+
+#Exercise 3.4.3
+def do_twice(f):
+    f()
+    f()
+ 
+
+def do_twice(f):
+    f()
+    f()
+
+def print_twice():
+    print 'spam'
+
+do_twice(print_twice)
+
+# Exercise 3.4.4
+def print_twice(f, arg):
+    f(arg)
+    f(arg)
+
+def print_spam(arg):
+    print arg
+
+print_twice(print_spam, 'spam')
+
+#Exercise 3.4.5
+do_twice(f, arg):
+    f(arg)
+    f(arg)
+
+def print_twice(arg):
+    print arg
+    print arg
+
+do_twice(print_twice, 'spam')
+print ''
+
+def do_four(f, arg):
+    do_twice(f, arg)
+    do_twice(f, arg)
+
+do_four(print_twice, 'spam')
+print ''
